19accf1b1459519405f713f54c5eae07a780c74a
Emilha <66671961+Emilha@users.noreply.github.com>
2026-01-16 14:08:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ st.write("The name of your smoothie will be", name_of_order)
 #st.write("Your favorite fruit is:", option)
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEAERCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
